bf01843d354d3c259be1cdc3735b134dd992d755
Junhee Ko <jko@rainist.com>
2021-04-16 23:45:49 +0900

    Add ProxyImage class

---
+++ src/main/java/me/jko/ProxyImage.java
@@ -0,0 +1,20 @@
+package me.jko;
+
+class ProxyImage implements Image {
+
+  private String filename;
+  private Image image;
+
+  public ProxyImage(String filename) {
+    this.filename = filename;
+  }
+
+  @Override
+  public void displayImage() {
+    if (image == null) {
+      image = new RealImage(filename);
+    }
+
+    image.displayImage();
+  }
+}
